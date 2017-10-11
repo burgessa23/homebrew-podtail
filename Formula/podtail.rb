cdc15757bc5b003501e9e4ec49ff5f36a4f88431
John McCabe <john@johnmccabe.net>
2017-10-11 14:26:33 +0100

    Bump to v0.1.2

--- Formula/podtail.rb
+++ Formula/podtail.rb
@@ -1,9 +1,9 @@
 class Podtail < Formula
     desc "Tool to tail Kubernetes logs from multiple pods at the same time"
     homepage "https://github.com/johnmccabe/podtail/"
-    url "https://github.com/johnmccabe/podtail/releases/download/v0.1.1/podtail-darwin.tgz"
-    sha256 "854214d7a4f18640eadb7821645940c3c7138b3539433db57ea6a37623a54418"
-    version "v0.1.1"
+    url "https://github.com/johnmccabe/podtail/releases/download/v0.1.2/podtail-darwin.tgz"
+    sha256 "b82cefd4a8e1868201dd54a0edfe094ac65cb50305e01bbf78c491c1c0a9d7fd"
+    version "v0.1.2"
   
     bottle :unneeded
   
